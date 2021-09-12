6c25f985f725fc45acdb1d2dd63addfd29350e1c
oskarsja <oskarsja@gmail.com>
2021-09-12 22:50:54 +0300

    2. uzdevums

--- Python_101/109_sets_tuples.py
+++ Python_101/109_sets_tuples.py
@@ -14,22 +14,17 @@
 # ienākošā sequence var būt tuple vai list ar vienāda tipa vērtībām, vai pat string.
 
 
-# 2. Kopējie Elementi
-# Uzrakstiet funkciju,kas atgriež tuple ar kopējiem elementiem trijās virknēs. Virknes var būt list,tuple,string.
-# get_common_elements(seq1,seq2,seq3)
+# Uzrakstiet funkciju, kas spēj apstrādat patvalīgu skaitu virkņu un atgriež tuple ar kopējiem elementiem trijās virknēs. Virknes var būt list, tuple, string.
 # get_common_elements("abc",['a','b'],('b','c')) -> ('b',) # tuple are vienu element šim elementam seko komats
-# # atceramies, ka mēs varam pārveidot virknes uz set ar set(virkne), un set uz tuple ar tuple(myset)
-# PS Tiem, kas nav pirmo reizi ar pīpi uz jumta, padomāsim, vai varam uztaisīt funkciju, kas spēj apstrādat patvalīgu skaitu virkņu
+def get_common_elements(*args):
+    return tuple(set.intersection(*map(set, args)))
 
-
-
+print(get_common_elements("abc",['a','b'],('b','c')))
 
 # 3. Vai ir pangramma?
-# uzrakstīt funkciju is_pangram, kas atgriež True, kad mytext parametrs satur visus burtus kas padoti a alfabetā.
-# Savadāk atgriežam False.
-# pangramma - teikums,vārdu virkne, kas satur visus alfabeta burtus - https://en.wikipedia.org/wiki/Pangram
-# Atstarpes ignorējam,un uzskatam ka lielais burts ir tikpat derīgs kā mazais, t.i. šeit A un a -> a
-# PS. ar šo funkciju tad mēs varēsim pārbaudīt arī latviešu vai citu valodu pangramas, padodot a parametrā attiecīgā alfabēta burtus.
+# Uzrakstīt funkciju is_pangram, kas atgriež True, kad mytext parametrs satur visus burtus kas padoti a alfabetā. Savadāk atgriežam False.
+# Pangramma - teikums,vārdu virkne, kas satur visus alfabeta burtus.
+# Atstarpes ignorējam, un uzskatam ka lielais burts ir tikpat derīgs kā mazais.
 # def is_pangram(mytext, a = 'abcdefghijklmnopqrstuvwxyz'):
 #     return str(mytext.lower()) >= a
 
